172c2ff890d726ed6d904bb53fe68a901cd5e75f
Shanmukh Das <shanmukhxd@outlook.com>
2026-07-06 15:59:48 +0530

    Added random computer engine

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class Game{
     void printBoard(char board[][]){
         for(int i=0;i<3;i++){
@@ -7,6 +8,7 @@ class Game{
             System.out.println();
         }
     }
+
     boolean checkWin(char board[][], char player){
         for(int i = 0; i<3; i++){
             if(board[i][0] == player && board[i][1] == player && board[i][2] == player)
@@ -23,11 +25,21 @@ class Game{
             return true;
         return false;
     }
+
+    int[] randomMove(char board[][]){
+        Random rand = new Random();
+        while(true){
+            int row = rand.nextInt(3);
+            int col = rand.nextInt(3);
+            if(board[row][col] == '-')
+                return new int[]{row, col};
+        }
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Game ob = new Game();
 
-        //initialize:
         char board[][] = new char[3][3];
         for(int i = 0; i < 3; i++){
             for(int j = 0; j<3; j++)
@@ -35,16 +47,31 @@ class Game{
         }
         ob.printBoard(board);
 
+        System.out.println("Play against computer? (y/n): ");
+        char mode = sc.next().charAt(0);
+        boolean vsComputer;
+        if(mode=='y'||mode=='Y')
+            vsComputer = true;
+        else
+            vsComputer = false;
+
         char current = 'X';
         int moves = 0;
 
         while(true){
-            //input:
-            System.out.println("Player "+ current+", enter row and column no. (both 0-2): ");
-            int row = sc.nextInt();
-            int col = sc.nextInt();
+            int row, col;
 
-            //validate:
+            if(current == 'O' && vsComputer){
+                int[] move = ob.randomMove(board);
+                row = move[0];
+                col = move[1];
+                System.out.println("Computer plays: "+row+" "+col);
+            } else {
+                System.out.println("Player "+ current+", enter row and column no. (both 0-2): ");
+                row = sc.nextInt();
+                col = sc.nextInt();
+            }
+
             if(row<0 || row>2 || col<0 || col>2){
                 System.out.println("Invalid input");
                 continue;
@@ -59,21 +86,18 @@ class Game{
                 ob.printBoard(board);
             }
 
-            //check win:
             if(ob.checkWin(board, current)){
                 ob.printBoard(board);
                 System.out.println("Player "+ current+", won!");
                 break;
             }
 
-            //check draw:
             if(moves == 9){
                 ob.printBoard(board);
                 System.out.println("Game draw");
                 break;
             }
 
-            //switch player:
             if(current == 'X')
                 current = 'O';
             else
