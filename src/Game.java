d5f673d33b728eb83df382a030323875bdaec840
Shanmukh Das <shanmukhxd@outlook.com>
2026-07-06 15:53:35 +0530

    Fixed ArrayIndexOutOfBoundsException

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class Game{
     void printBoard(char board[][]){
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++)
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++)
                 System.out.print(board[i][j]+" ");
             System.out.println();
         }
